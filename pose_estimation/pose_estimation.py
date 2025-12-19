c358555a214c23d2dd8f5b5d7909e715d7db0fcc
isaaal <faizalrahmancontact@gmail.com>
2025-12-19 16:59:31 +0700

    No Changes

--- pose_estimation/pose_estimation.py
+++ pose_estimation/pose_estimation.py
@@ -8,12 +8,12 @@ def calculate_angle(a, b, c):
     Calculates the angle between three points.
     a, b, c: (x, y) coordinates
     b is the vertex of the angle.
-    Returns angle in degrees.
+    Returns angle in degrees. a
     """
     a = np.array(a) # First
     b = np.array(b) # Mid
-    c = np.array(c) # End
-    
+    c = np.array(c) # End 
+
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
     
